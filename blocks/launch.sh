7e850521dd00eed97945b23a60792f19db9723a2
ngynLk <nguyenleluc@gmail.com>
2020-04-23 23:48:23 +0200

    some updates of blocks and add san bar

--- blocks/launch.sh
+++ blocks/launch.sh
@@ -6,6 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# polybar -rq dummy & 
 polybar -rq music &
 polybar -rq tray &
 polybar -rq i3 &
--- san/launch.sh
+++ san/launch.sh
@@ -6,6 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -rq bar &
+polybar -r bar &
 
 echo "Polybar launched..."
