83b4449c548c03615d0c58a3bf03e1531490f63b
ngynLk <nguyenleluc@gmail.com>
2020-11-11 13:16:22 +0100

    update arch-blur with playerctl

---
+++ material_onethick/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar -r base &
+echo "Polybar launched..."
